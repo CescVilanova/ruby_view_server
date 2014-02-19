37f12d85c297da7af62a17df297a61a981d43d29
CescVilanova <vfrancesc@gmail.com>
2014-02-19 13:33:57 +0000

    Exercise 4 completed.

--- server_simple.rb
+++ server_simple.rb
@@ -5,10 +5,14 @@ require 'webrick'
 root = File.expand_path 'public'
 
 # Create a new server on port 8000
-server = WEBrick::HTTPServer.new :Port => 8000, :DocumentRoot => root
+server = WEBrick::HTTPServer.new :Port => 3000, :DocumentRoot => root
 
 ## Capture control+c to shut down the server
 trap 'INT' do server.shutdown end
+
+server.mount_proc '/' do |req, res|
+  res.body = 'Hello, world!'
+end
 
 ## Start the server
 server.start
